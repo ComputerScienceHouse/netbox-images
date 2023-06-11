ca01678149c75cca3eb6a58915ab5a59bf9632c2
Mary Strodl <ipadlover8322@gmail.com>
2023-06-11 10:43:00 -0400

    migrate: clear cache and reindex search

--- migrate.sh
+++ migrate.sh
@@ -5,4 +5,6 @@
 python3 netbox/manage.py migrate
 python3 netbox/manage.py trace_paths --no-input
 python3 netbox/manage.py remove_stale_contenttypes --no-input
+python3 netbox/manage.py reindex --lazy
 python3 netbox/manage.py clearsessions
+python3 netbox/manage.py clearcache
