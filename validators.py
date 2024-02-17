e4049872ded5975fc96239bcf19c81da255c8b7d
Joe Abbate <josephabbateny@gmail.com>
2024-02-16 23:19:05 -0500

    Did the return break it?

--- validators.py
+++ validators.py
@@ -7,27 +7,21 @@ class DNSValidator(CustomValidator):
             records = Record.objects.filter(name=instance.name, zone=instance.zone, type=RecordTypeChoices.A)
             if records.exists() and records[0].ipam_ip_address.assigned_object_id != instance.ipam_ip_address.assigned_object_id:
                 self.fail("iPv4 Record for this name exists on a different interface!", field='name')
-                return
             records = Record.objects.filter(name=instance.name, zone=instance.zone, type=RecordTypeChoices.CNAME)
             if records.exists():
                 self.fail("Record for this name in this zone already exists!", field='name')
-                return
         if instance.type == RecordTypeChoices.A:
             records = Record.objects.filter(name=instance.name, zone=instance.zone, type=RecordTypeChoices.AAAA)
             if records.exists() and records[0].ipam_ip_address.assigned_object_id != instance.ipam_ip_address.assigned_object_id:
                 self.fail("iPv6 Record for this name exists on a different interface!", field='name')
-                return
             records = Record.objects.filter(name=instance.name, zone=instance.zone, type=RecordTypeChoices.CNAME)
             if records.exists():
                 self.fail("Record for this name in this zone already exists!", field='name')
-                return
         if instance.type == RecordTypeChoices.CNAME:
             records = Record.objects.filter(name=instance.name, zone=instance.zone, type__in=[RecordTypeChoices.A, RecordTypeChoices.AAAA])
             if records.exists():
                 self.fail("Record for this name in this zone already exists!", field='name')
-                return
         records = Record.objects.filter(name=instance.name, zone=instance.zone, type=instance.type)
         if records.exists():
             self.fail("Record of given type for this name in this zone already exists!", field='name')
-            return
 
