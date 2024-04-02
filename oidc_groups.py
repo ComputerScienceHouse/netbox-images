2e15191c4f480271805ce65a92961f2e63dc7aae
Cecilia Lau <cecilialau6776@gmail.com>
2024-04-02 01:57:45 -0400

    better group mirroring

--- oidc_groups.py
+++ oidc_groups.py
@@ -2,13 +2,18 @@ from django.contrib.auth.models import Group
 
 
 def oidc_groups_handler(strategy, response, user, *args, **kwargs):
-    # clone groups from LDAP
-    groups = Group.objects.filter(name__in=response["groups"])
-    user.groups.add(*groups)
+    # mirror groups
+    groups = [
+        Group.objects.get_or_create(name=group)[0] for group in response["groups"]
+    ]
+    user.groups.set(groups)
 
     # give active rtps superuser and staff
     is_active_rtp = "active_rtp" in response["groups"]
     user.is_superuser = is_active_rtp
     user.is_staff = is_active_rtp
 
+    user.is_active = "member" in response["groups"]
+
+    # save
     strategy.storage.user.changed(user)
