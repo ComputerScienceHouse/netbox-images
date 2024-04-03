339c2d6fe860545bc09fbf4289bbf797592ec6e7
Cecilia Lau <cecilialau6776@gmail.com>
2024-04-02 20:36:37 -0400

    fix migrations

--- migrate.sh
+++ migrate.sh
@@ -7,4 +7,3 @@ python3 netbox/manage.py trace_paths --no-input
 python3 netbox/manage.py remove_stale_contenttypes --no-input
 python3 netbox/manage.py reindex --lazy
 python3 netbox/manage.py clearsessions
-python3 netbox/manage.py clearcache
