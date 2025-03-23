80edfd46162cb1efa9f7e7803cc4d1a41f1f0cc9
Cecilia Lau <cecilialau6776@gmail.com>
2025-03-23 16:26:24 -0400

    uncomment group mirroring

--- oidc_groups.py
+++ oidc_groups.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import Group
 
 def oidc_groups_handler(strategy, response, user, *args, **kwargs):
     # mirror groups
-    #groups = [
-    #    Group.objects.get_or_create(name=group)[0] for group in response["groups"]
-    #]
-    #user.groups.set(groups)
+    groups = [
+        Group.objects.get_or_create(name=group)[0] for group in response["groups"]
+    ]
+    user.groups.set(groups)
 
     # give active rtps superuser and staff
     is_active_rtp = "active_rtp" in response["groups"]
